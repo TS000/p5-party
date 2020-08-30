33880812b87aee096b3a077e20873ce2a349c59a
Tyler S <ghost@Tylers-Mac-mini.local>
2020-08-29 22:01:32 -0600

    line change

--- maze.js
+++ maze.js
@@ -1,14 +1,15 @@
 let x = 0;
 let y = 0;
-let spacing = 20;
+let spacing = 30; 
 function setup() {
   createCanvas(700, 700);
   background(0);
 }
 
 function draw() {
+  
  stroke('red');
-  strokeWeight(10)
+  strokeWeight(8)
   if (random(1) < 0.8) {
   line(x, y, x+ spacing, y + spacing)
   } else {
